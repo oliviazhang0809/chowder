9da858b46a455cb9d063ffac878808dae256b1e1
Olivia Zhang <tzhang1@paypal.com>
2014-12-12 16:00:50 -0800

    with change

--- myVagrantTestPool/README.md
+++ myVagrantTestPool/README.md
@@ -1,4 +1,4 @@
-This is a sweet simple multivagrant program that create centOS boxes.
-You can specify the number of boxes in Vagrantfile by setting number n.
+This is a sweet simple multivagrant program that uses centOS boxes.
+By default it creates 3 boxes with specified hostname, role, ip (set as private network), autostart (so you can set if the box can be brought up automatically by vagrant up) and size of ram.
 
-I also include a helloworld module so that you can test if master/client can talk to each other in an appropriate way.
+I also include a helloworld module so that you can test if master/client can talk to each other in an appropriate way. But you don't have to use it.
--- myVagrantTestPool/scripts/setup.sh
+++ myVagrantTestPool/scripts/setup.sh
@@ -1,18 +1,3 @@
 echo "Installing java"
-cd /opt
-sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz" -O /opt/jdk-7u71-linux-x64.tar.gz
-sudo tar xvf /opt/jdk-7u71-linux-x64.tar.gz
-sudo chown -R root: jdk1.7.0_71
-sudo alternatives --install /usr/bin/java java /opt/jdk1.7.0_71/bin/java 1
-sudo alternatives --install /usr/bin/javac javac /opt/jdk1.7.0_71/bin/javac 1
-sudo alternatives --install /usr/bin/jar jar /opt/jdk1.7.0_71/bin/jar 1
+rpm -Uvh http://stingray-vagrant.stratus.dev.ebay.com/packages/rpm/jdk-7u51-linux-x64.rpm
 java -version
-
-# install puppet gem files
-gem install -q -v=3.7.2 --no-rdoc --no-ri puppet
-gem install -v=2.2.8 --no-rdoc --no-ri CFPropertyList
-gem install -q -v=1.1.1 --no-rdoc --no-ri hiera-file
-gem install -q -v=1.0.1 --no-rdoc --no-ri deep_merge
-
-puppet agent --enable
