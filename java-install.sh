65eb2344553c22f650d7a2e1b3aebf3773bddad6
Olivia Zhang <tzhang1@paypal.com>
2014-12-16 16:18:29 -0800

    on centOS

--- java-install.sh
+++ java-install.sh
@@ -1,4 +1,4 @@
-echo "Installing java"
+echo "Installing java on centOS..."
 cd /opt
 sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 "http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz" -O /opt/jdk-7u71-linux-x64.tar.gz
