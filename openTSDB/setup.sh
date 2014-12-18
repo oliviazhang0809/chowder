66ba4e1dfaeaf1d7270b9a5112991f4c043630b4
Olivia Zhang <zhangtian0809@gmail.com>
2014-12-18 00:50:27 -0800

    standalone java script

--- openTSDB/setup.sh
+++ openTSDB/setup.sh
@@ -1,3 +1,7 @@
+# install java7
+wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/java-7-install.sh --no-check-certificate
+sudo /bin/bash java-7-install.sh
+
 echo "installing hbase"
 wget https://archive.apache.org/dist/hbase/hbase-0.94.24/hbase-0.94.24.tar.gz
 tar xzvf hbase-0.94.24.tar.gz
@@ -5,3 +9,13 @@ tar xzvf hbase-0.94.24.tar.gz
 mkdir -p /usr/local/hbase
 cp -r hbase-0.94.24/* /usr/local/hbase
 
+# TODO configure hbase
+# TODO start hbase
+
+echo "installing gnuplot"
+sudo rm /etc/yum.repos.d/CentOS-Base.repo
+wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/openTSDB/CentOS-Base.repo -O /etc/yum.repos.d/CentOS-Base.repo --no-check-certificate
+yum -y install gnuplot
+
+echo "installing openTSDB"
+rpm -Uvh https://github.com/OpenTSDB/opentsdb/releases/download/v2.1.0RC1/opentsdb-2.1.0RC1.noarch.rpm
--- vagrant-multi-vm-pool/scripts/setup.sh
+++ vagrant-multi-vm-pool/scripts/setup.sh
@@ -1,13 +1,5 @@
-echo "Installing java"
-cd /opt
-sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz" -O /opt/jdk-7u71-linux-x64.tar.gz
-sudo tar xvf /opt/jdk-7u71-linux-x64.tar.gz
-sudo chown -R root: jdk1.7.0_71
-sudo alternatives --install /usr/bin/java java /opt/jdk1.7.0_71/bin/java 1
-sudo alternatives --install /usr/bin/javac javac /opt/jdk1.7.0_71/bin/javac 1
-sudo alternatives --install /usr/bin/jar jar /opt/jdk1.7.0_71/bin/jar 1
-java -version
+wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/java-7-install.sh --no-check-certificate
+sudo /bin/bash java-7-install.sh
 
 echo "installing hbase"
 wget https://archive.apache.org/dist/hbase/hbase-0.94.24/hbase-0.94.24.tar.gz
