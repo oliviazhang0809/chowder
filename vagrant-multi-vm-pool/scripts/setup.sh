d48978d7a19b91f17e7387dfa91f6203358b52f4
Olivia Zhang <tzhang1@paypal.com>
2014-12-18 10:10:51 -0800

    opentsdb vagrant program

--- vagrant-multi-vm-pool/scripts/setup.sh
+++ vagrant-multi-vm-pool/scripts/setup.sh
@@ -1,9 +1,3 @@
 wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/java-install.sh --no-check-certificate
 sudo /bin/bash java-install.sh
 
-echo "installing hbase"
-wget https://archive.apache.org/dist/hbase/hbase-0.94.24/hbase-0.94.24.tar.gz
-tar xzvf hbase-0.94.24.tar.gz
-rm hbase-0.94.24.tar.gz
-mkdir -p /usr/local/hbase
-cp -r hbase-0.94.24/* /usr/local/hbase
