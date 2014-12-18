097e80849ce2f80586bc3c456ff9ab6d18aca568
olivia zhang <oliviazhang0809@gmail.com>
2014-12-18 01:59:54 -0800

    add script for openTSDB

---
+++ openTSDB/hbase-env.sh
@@ -0,0 +1,2 @@
+export HBASE_OPTS="-XX:+UseConcMarkSweepGC"
+export JAVA_HOME=/usr
--- openTSDB/setup.sh
+++ openTSDB/setup.sh
@@ -1,16 +1,25 @@
 # install java7
 wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/java-install.sh --no-check-certificate
 sudo /bin/bash java-install.sh
+rm java-install.sh
+
+# install default
+yum -y install git
 
 echo "installing hbase"
 wget https://archive.apache.org/dist/hbase/hbase-0.94.24/hbase-0.94.24.tar.gz
 tar xzvf hbase-0.94.24.tar.gz
- rm hbase-0.94.24.tar.gz
+rm hbase-0.94.24.tar.gz
 mkdir -p /usr/local/hbase
 cp -r hbase-0.94.24/* /usr/local/hbase
 
-# TODO configure hbase
-# TODO start hbase
+#configure hbase
+rm hbase-0.94.24/conf/hbase-env.sh
+wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/openTSDB/hbase-env.sh -O hbase-0.94.24/conf/hbase-env.sh --no-check-certificate
+rm hbase-0.94.24/conf/hbase-site.xml
+wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/openTSDB/hbase-site.xml -O hbase-0.94.24/conf/hbase-site.xml --no-check-certificate
+echo "starting hbase"
+hbase-0.94.24/bin/start-hbase.sh
 
 echo "installing gnuplot"
 sudo rm /etc/yum.repos.d/CentOS-Base.repo
@@ -18,4 +27,10 @@ wget https://raw.githubusercontent.com/oliviazhang0809/grafana/master/openTSDB/C
 yum -y install gnuplot
 
 echo "installing openTSDB"
-rpm -Uvh https://github.com/OpenTSDB/opentsdb/releases/download/v2.1.0RC1/opentsdb-2.1.0RC1.noarch.rpm
+yum -y install install autoconf automake libtool
+git clone git://github.com/OpenTSDB/opentsdb.git
+cd opentsdb
+./build.sh
+
+env COMPRESSION=NONE HBASE_HOME=/home/vagrant/hbase-0.94.24 ./src/create_table.sh
+
